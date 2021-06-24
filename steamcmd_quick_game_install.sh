572dcd2d4136e3b376a5fd1b82cb3c44903afa9d
tylermwight@gmail.com <tylermwight@gmail.com>
2021-06-24 08:13:29 +0000

    add ability to change install location

--- steamcmd_quick_game_install.sh
+++ steamcmd_quick_game_install.sh
@@ -3,11 +3,14 @@
 #Tyler Wight
 #Change these two variables to install any game. The name will be used for the folder structure and the user that is created.
 #=====================
-gamename=valheim
+gamename=valheim2
 gamesteamcode=896660
+installpath="/home/valheim2"
 #=====================
 
-cronjob="*/30 * * * * /mnt/${gamename}/backup.sh"
+
+fullpath="${installpath}/${gamename}"
+cronjob="*/30 * * * * ${fullpath}/backup.sh"
 sudo adduser $gamename
 sudo usermod -aG sudo $gamename
 sudo apt-get update -y && sudo apt-get upgrade -y
@@ -19,18 +22,20 @@ sudo dpkg --add-architecture i386
 sudo apt-get update -y
 sudo apt install lib32gcc1 steamcmd -y
 sudo ln -s /usr/games/steamcmd steamcmd
-sudo mkdir /mnt/$gamename
-sudo mkdir /mnt/$gamename/bak
+sudo mkdir -p $fullpath
+sudo mkdir "${fullpath}/bak"
 
-#sudo cp -n ./backup.sh /mnt/$gamename/backup.sh
+#if you write a backup script for your game you can have it copy the script to the install directory, and use the cronjob settings to make them automatic
+#sudo cp -n ./backup.sh $fullpath/backup.sh
 
-sudo chown -R $gamename:$gamename /mnt/$gamename
-sudo su -c "steamcmd +login anonymous +force_install_dir /mnt/${gamename} +app_update ${gamesteamcode} validate +exit" -s /bin/sh $gamename
+sudo chown -R $gamename:$gamename $fullpath
+sudo su -c "steamcmd +login anonymous +force_install_dir ${fullpath} +app_update ${gamesteamcode} validate +exit" -s /bin/sh $gamename
 
+#enable the below line to add the contents of the cronjob variable to the crontab of the user, this preserves all existing crontab entries
 #sudo su -c "(crontab -l| grep -v -F '$cronjob'; echo '$cronjob') | crontab -" -s /bin/sh $gamename
 
 
 echo "The game ${gamename} has been installed or updated  via steamcmd."
-echo "During the insallation we created a user named ${gamename} and installed the game to location /mnt/${gamename}"
+echo "During the insallation we created a user named ${gamename} and installed the game to location ${fullpath}"
 echo "The steam game code used to download from steam CMD was ${gamesteamcode}"
 echo "You can run this script again to update the game."
